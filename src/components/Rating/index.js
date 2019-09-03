e8cf23b64292385f3713d01ca98251f06790530a
Diljit VJ <diljitvj@gmail.com>
2019-09-03 16:32:40 +0530

    Add hover behaviour for rating component

--- src/components/Rating/index.js
+++ src/components/Rating/index.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./style.module.css";
 import StarOutlineIcon from "@material-ui/icons/StarBorder";
 import StarFilledIcon from "@material-ui/icons/Star";
 import StarHalfFilledIcon from "@material-ui/icons/StarHalfOutlined";
 import clsx from "clsx";
+import { debounce } from "lodash";
 
-const Rating = ({ total, value, starSize = "medium" }) => {
+const Rating = ({ total, value, starSize = "medium", onChange }) => {
   const ratings = [];
+  const [hoverValue, setHoverValue] = useState(value);
+
+  const setValue = debounce(setHoverValue, 10);
+
+  const handleOnChange = value => {
+    onChange({ target: { value } });
+  };
+
   for (let i = 1; i <= total; i++) {
-    if (i <= value) {
+    if (i <= hoverValue) {
       ratings.push(
-        <StarFilledIcon className={clsx(styles[starSize], styles.star)} />
+        <StarFilledIcon
+          className={clsx(styles[starSize], styles.star)}
+          onClick={() => handleOnChange(i)}
+          onMouseOver={() => {
+            setValue(i);
+          }}
+          onMouseLeave={() => {
+            setValue(value);
+          }}
+        />
       );
     } else if (i - value === 0.5) {
       ratings.push(
@@ -18,12 +36,25 @@ const Rating = ({ total, value, starSize = "medium" }) => {
       );
     } else {
       ratings.push(
-        <StarOutlineIcon className={clsx(styles[starSize], styles.star)} />
+        <StarOutlineIcon
+          className={clsx(styles[starSize], styles.star)}
+          onClick={() => handleOnChange(i)}
+          onMouseOver={() => {
+            setValue(i);
+          }}
+          onMouseLeave={() => {
+            setValue(value);
+          }}
+        />
       );
     }
   }
 
-  return <div className={styles.wrapper}>{ratings}</div>;
+  return (
+    <div className={styles.wrapper} onMouseLeave={() => setValue(value)}>
+      {ratings}
+    </div>
+  );
 };
 
 export default Rating;
